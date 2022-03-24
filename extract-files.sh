36a6836549348e5e483447010fec888764bdf0e8
fuckdedeurkruk <marco_veldhuizen@gotmail.com>
2022-03-24 18:14:58 +0100

    sm8350-common: extract-files: Patch camera postproc impl
    Don't call configureRpcThreadpool for camera postproc impl
    
     * In Android 12, no longer allowed to shrink the process pool after a
       larger process pool is started, we should patch camera postproc impl
       to nuke it from shrinking process pool and prevent triggering a fatal
       to cause the camera to crash.
       Ref: https://android.googlesource.com/platform/system/libhwbinder/+/8e047f7a626c782a02dc9e67dee2dbae35bc6956.
    
     * The following are the difference between before and after the
       patch.
       Before:
       MOV    W1, #1
       BL     ._ZN7android8hardware22configureRpcThreadpoolEmb
       Now:
       MOV    W1, #1
       NOP
    
     * Obtained by comparing the camera postproc impl modified by
       GitHub@erfanoabdi.
    
     * A tutorial on use hexdump, sed and xxd to patch hex in
       binary file: https://everydaywithlinux.blogspot.com/2012/11/patch-strings-in-binary-files-with-sed.html.
    
    Co-authored-by: Erfan Abdi <erfangplus@gmail.com>
    Signed-off-by: TH779 <i@779.moe>
    Change-Id: I66c257e80bf56a48c640efc3c1c8949852edf756
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -63,6 +63,10 @@ function blob_fixup() {
         system_ext/lib64/libwfdnative.so)
             sed -i "s/android.hidl.base@1.0.so/libhidlbase.so\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00/" "${2}"
             ;;
+        vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so)
+            hexdump -ve '1/1 "%.2X"' "${2}" | sed "s/210080521F0A0094/210080521F2003D5/g" | xxd -r -p > "${TMPDIR}/${1##*/}"
+            mv "${TMPDIR}/${1##*/}" "${2}"
+            ;;
     esac
 }
 
