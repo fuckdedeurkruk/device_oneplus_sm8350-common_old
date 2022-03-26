f9e1745e57186f4dc9a806bd5818c8081fe0e6f7
fuckdedeurkruk <marco_veldhuizen@gotmail.com>
2022-03-26 22:33:11 +0100

    cleanup common

--- common.mk
+++ common.mk
@@ -289,25 +289,12 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.mapper@2.0.vendor \
     vendor.qti.hardware.display.mapper@3.0.vendor \
     vendor.qti.hardware.display.mapper@4.0.vendor
-
-# DRM
-PRODUCT_PACKAGES += \
-    android.hardware.drm@1.3.vendor \
-    android.hardware.drm@1.4-service.clearkey
     
 # DeviceSettings
 PRODUCT_PACKAGES += \
     DeviceSettings   
 
-# Dolby
-PRODUCT_PACKAGES += \
-    OPSoundTunerOverlay
-
 # DRM
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/dolby/dax-default.xml:$(TARGET_COPY_OUT_ODM)/etc/dolby/dax-default.xml
-
-# DeviceSettings
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor \
     android.hardware.drm@1.4-service.clearkey
