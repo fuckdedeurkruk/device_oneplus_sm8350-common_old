b8ad2ceadd3e66e71c1a212b495dd823b38c8c29
fuckdedeurkruk <fuckdedeurkruk@users.noreply.github.com>
2022-05-14 13:47:55 +0200

    sm8350-common: BoardConfig: Remove virtual framebuffer
     * It's largely for testing purposes and it consumes
       a large amount of memory. If your hardware doesn't
       have graphics then it can be useful, which isn't the
       case for shipped devices.
    
    Change-Id: I51becacd30d17e71cdeb0fbe31bbe1b94262b316
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -62,9 +62,7 @@ androidboot.selinux=permissive \
     reboot=panic_warm \
     service_locator.enable=1 \
     swiotlb=2048 \
-    video=vfb:640x400,bpp=32,memsize=3072000 \
     kpti=off
-    
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
