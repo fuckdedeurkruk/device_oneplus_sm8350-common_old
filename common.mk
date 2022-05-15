309b9fdd5e78af099bd3a39e595feb51ee4f5b62
fuckdedeurkruk <fuckdedeurkruk@users.noreply.github.com>
2022-05-15 20:33:36 +0200

    introduce some prebuilt packages

--- common.mk
+++ common.mk
@@ -32,6 +32,9 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/oneplus/sm8350-common/sm8350-common-vendor.mk)
 
+# Inherit Prebuilt Packages from vendor/oneplus/addons/apps
+$(call inherit-product, vendor/oneplus/addons/apps/apps.mk)
+
 # Additional native libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
